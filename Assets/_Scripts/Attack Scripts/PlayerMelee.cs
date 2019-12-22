039d7671808ae2833f4524a5e20a9c692ca04663
Brian <brianj.pajak@gmail.com>
2019-12-21 19:53:23 -0500

    Add Player Melee system

---
+++ Assets/_Scripts/Attack Scripts/PlayerMelee.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMelee : MonoBehaviour
+{
+    [SerializeField]Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponentInChildren<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            anim.SetTrigger("MeleeAttack");
+        }
+    }
+}
