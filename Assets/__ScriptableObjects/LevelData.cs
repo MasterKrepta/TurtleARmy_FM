b1f867b263bb2074a32799e8d5b6f16e42bec82c
Brian <brianj.pajak@gmail.com>
2020-02-06 20:58:40 -0500

    Upgrade unity 2019.3 and UI scale change
    preliminary graphics added to UI

---
+++ Assets/__ScriptableObjects/LevelData.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewLevelData", menuName = "ScriptableObjects/Level", order = 3)]
+public class LevelData : ScriptableObject
+{
+    public string LevelName = "New Level";
+    public GameObject[] EnemyTypes;
+    public float SpawnRate = 10f;
+    public int BaseHealth = 200;
+
+}
