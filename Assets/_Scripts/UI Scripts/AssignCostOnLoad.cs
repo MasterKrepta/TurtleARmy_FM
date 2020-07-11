c5d23b375436716ad99855028dac72ddbcb06a26
Brian <brianj.pajak@gmail.com>
2020-07-11 12:21:07 -0400

    Added cost to UI button text

---
+++ Assets/_Scripts/UI Scripts/AssignCostOnLoad.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class AssignCostOnLoad : MonoBehaviour
+{
+    [SerializeField] Minion minion;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        GetComponent<Text>().text = minion.CostToSpawn.ToString();
+    }
+
+
+}
