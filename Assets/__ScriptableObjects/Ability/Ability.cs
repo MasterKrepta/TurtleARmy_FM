f60328773c3eba8fe5a11921a5ff309e0eabab35
Brian <brianj.pajak@gmail.com>
2020-09-23 21:04:50 -0400

    Control spawning of abilities via string interpolation.

--- Assets/__ScriptableObjects/Ability/Ability.cs
+++ Assets/__ScriptableObjects/Ability/Ability.cs
@@ -1,10 +1,20 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "New Ability", menuName = "ScriptableObjects/Abilities", order = 3)]
 public class Ability : ScriptableObject
 {
+    public enum AbilityTypes
+    { 
+        Attack,
+        Air,
+        Edge
+    }
+
+
+    public AbilityTypes Type = AbilityTypes.Attack;
     public bool CanUse;
     public string Name = "New Ability";
     public float Cost = 1f;
@@ -16,6 +26,7 @@ public class Ability : ScriptableObject
 
     public void UseAbility()
     {
+        AbilitySpawnPoint = GameObject.Find($"{Type.ToString()}SpawnPoint").transform; //TODO how inefficient is this
         Instantiate(AbilityPrefab, AbilitySpawnPoint.position, Quaternion.identity);
         Debug.Log($"{Name} has been fired");
     }
