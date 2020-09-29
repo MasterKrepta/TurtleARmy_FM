d1618a765bb8cdc1f28588529c056feb3996f886
Brian <brianj.pajak@gmail.com>
2020-09-28 21:10:34 -0400

    Ion Cannon ability

---
+++ Assets/_Scripts/Ability Scripts/AIonCannon.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIonCannon : MonoBehaviour
+{
+    public float fireTime = 1f;
+    public float dmgAmount = 1f;
+    public float moveSpeed = 1.25f;
+
+
+    private void Awake()
+    {
+        Destroy(this.gameObject, fireTime);
+        transform.position = new Vector3(transform.position.x, 5.4f, transform.position.z);
+    }
+    private void Update()
+    {
+        transform.Translate(Vector3.forward * moveSpeed* Time.deltaTime);
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        IHasHealth otherHealth = other.GetComponent<IHasHealth>();
+
+        if (otherHealth != null && other.tag != gameObject.tag)
+        {
+            otherHealth.TakeDamage(dmgAmount);
+            //print($"{this.name} hits {other.name} and causes damage");
+            
+        }
+    }
+}
